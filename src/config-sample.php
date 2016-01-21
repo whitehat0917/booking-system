7a794a31d07fab5b13e708b0d1648197cec0492f
Senior Developer <whitehat0917@gmail.com>
2016-01-21 20:04:55 +0100

    Refactored the language config setting.

--- src/config-sample.php
+++ src/config-sample.php
@@ -1,23 +1,26 @@
 <?php
 /* ----------------------------------------------------------------------------
  * Easy!Appointments - Open Source Web Scheduler
- * 
+ *
  * @package     EasyAppointments
  * @author      A.Tselegidis <alextselegidis@gmail.com>
  * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
- * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3 
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        http://easyappointments.org
  * @since       v1.0.0
  * ---------------------------------------------------------------------------- */
 
 /**
- * Easy!Appointments Configuration File 
- * 
- * Set your installation BASE_URL * without the trailing slash * and the database 
+ * Easy!Appointments Configuration File
+ *
+ * Set your installation BASE_URL * without the trailing slash * and the database
  * credentials in order to connect to the database. You can enable the DEBUG_MODE
  * while developing the application.
- * 
- * IMPORTANT: 
+ *
+ * Set the default language by changing the LANGUAGE constant. For a full list of
+ * available languages look at the /application/config/config.php file.
+ *
+ * IMPORTANT:
  * If you are updating from version 1.0 you will have to create a new "config.php"
  * file because the old "configuration.php" is not used anymore.
  */
@@ -26,12 +29,13 @@ class Config {
     // General Settings
     // ------------------------------------------------------------------------
     const BASE_URL      = 'http://url-to-easyappointments-directory';
+    const LANGUAGE      = 'english';
     const DEBUG_MODE    = FALSE;
-     
+
     // ------------------------------------------------------------------------
     // Database Settings
     // ------------------------------------------------------------------------
-    const DB_HOST       = '';    
+    const DB_HOST       = '';
     const DB_NAME       = '';
     const DB_USERNAME   = '';
     const DB_PASSWORD   = '';
@@ -41,8 +45,8 @@ class Config {
     // ------------------------------------------------------------------------
     const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
     const GOOGLE_PRODUCT_NAME   = '';
-    const GOOGLE_CLIENT_ID      = ''; 
-    const GOOGLE_CLIENT_SECRET  = ''; 
+    const GOOGLE_CLIENT_ID      = '';
+    const GOOGLE_CLIENT_SECRET  = '';
     const GOOGLE_API_KEY        = '';
 }
 /* End of file config.php */
