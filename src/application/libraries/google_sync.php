bfd973f704a6a09160345781f391c62a4946a4c7
Senior Developer <whitehat0917@gmail.com>
2017-05-18 21:27:37 +0200

    Updated years in index.php file.

--- src/application/libraries/google_sync.php
+++ src/application/libraries/google_sync.php
@@ -5,7 +5,7 @@
  *
  * @package     EasyAppointments
  * @author      A.Tselegidis <alextselegidis@gmail.com>
- * @copyright   Copyright (c) 2013 - 2017, Alex Tselegidis
+ * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
  * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        http://easyappointments.org
  * @since       v1.0.0
@@ -351,10 +351,6 @@ class Google_Sync {
         $calendarList = $this->service->calendarList->listCalendarList();
         $calendars = array();
         foreach ($calendarList->items as $google_calendar) {
-            if ($google_calendar->getAccessRole() === 'reader') {
-                continue;
-            }
-
             $calendars[] = array(
                 'id' => $google_calendar->id,
                 'summary' => $google_calendar->summary
@@ -363,3 +359,6 @@ class Google_Sync {
          return $calendars;
     }
 }
+
+/* End of file google_sync.php */
+/* Location: ./application/libraries/google_sync.php */
