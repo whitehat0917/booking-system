124b510fa705bbf7de235a826c83ca236af6bbec
Senior Developer <whitehat0917@gmail.com>
2017-06-11 21:10:53 +0200

    Dedicated service or provider selections must not be disabled.

--- src/application/libraries/google_sync.php
+++ src/application/libraries/google_sync.php
@@ -5,7 +5,7 @@
  *
  * @package     EasyAppointments
  * @author      A.Tselegidis <alextselegidis@gmail.com>
- * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
+ * @copyright   Copyright (c) 2013 - 2017, Alex Tselegidis
  * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
  * @link        http://easyappointments.org
  * @since       v1.0.0
@@ -351,6 +351,10 @@ class Google_Sync {
         $calendarList = $this->service->calendarList->listCalendarList();
         $calendars = array();
         foreach ($calendarList->items as $google_calendar) {
+            if ($google_calendar->getAccessRole() === 'reader') {
+                continue;
+            }
+
             $calendars[] = array(
                 'id' => $google_calendar->id,
                 'summary' => $google_calendar->summary
@@ -359,6 +363,3 @@ class Google_Sync {
          return $calendars;
     }
 }
-
-/* End of file google_sync.php */
-/* Location: ./application/libraries/google_sync.php */
